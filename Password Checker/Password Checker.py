1fc047811a61999279a036423d10b47ef87c7fda
landoncevans <73617987+landoncevans@users.noreply.github.com>
2025-02-11 15:31:00 -0700

    added length

--- Password Checker/Password Checker.py
+++ Password Checker/Password Checker.py
@@ -2,7 +2,7 @@ import re
 
 def isValid(password: str) -> bool:
     rules = r"^(?=.*[A-Z])(?=.*!).*$"
-    if re.match(rules):
+    if re.match(rules, password) and  len(password) == 20:
         True
     else:
         False
